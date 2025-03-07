e7a17d146a97b0abc90cdf48303949ef37dd7926
gamersaround <martin.perret@gmail.com>
2025-03-07 18:43:26 +0100

    Add Redux store configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import productsReducer from './slices/productsSlice';
-import settingsReducer from './slices/settingsSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
-    settings: settingsReducer,
   },
 });
 
